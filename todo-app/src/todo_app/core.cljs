49b7857981bfc91f7582e0550de94be2d111d746
extellion <allan@guichard.biz>
2022-01-22 17:01:21 +0100

    chore: add documentation

--- todo-app/src/todo_app/core.cljs
+++ todo-app/src/todo_app/core.cljs
@@ -10,18 +10,24 @@
             [{:description "First item" :completed false}]))
 
 (defn home-page []
+  ;; value variable which will be able to be "mutated" 
+  ;; default state is empty string
   (let [value (r/atom "")]
   (fn []
   [:div
     [:h2 "Todo App"]
     [:form {:on-submit (fn [e]
+                        ;; usual prevent default
                         (.preventDefault e)
+                        ;; create a new todos array with new populated item and change todos with new array values
                         (swap! todos conj {:completed false :description @value})
+                        ;; reset the value of the input to empty string
                         (reset! value ""))}
       [:input {:type "text"
              :value @value
              :placeholder "Add new item"
              :on-change (fn [e]
+                          ;; get the value of the targeted element `e`
                           (reset! value (.-value (.-target e))))}]
     [:ul
       (for [todo @todos]
